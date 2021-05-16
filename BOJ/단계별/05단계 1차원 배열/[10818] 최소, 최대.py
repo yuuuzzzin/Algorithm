aceb17c3696fc33a39f73e6c2b98ab1366d71a63
yuuuzzzin <uiuiui118437@gmail.com>
2021-05-16 22:02:43 +0900

    [10818 최소, 최대] 문제 풀이

--- BOJ/단계별/05단계 1차원 배열/[10818] 최소, 최대.py
+++ BOJ/단계별/05단계 1차원 배열/[10818] 최소, 최대.py
@@ -1,4 +1,13 @@
-n = 4
-m = 3
-array = [[0] * 3 for _ in range(4)]
-print(array)
+a = [1, 4, 3]
+
+# 리스트 원소 삽입
+a.append(2)
+print("삽입: ", a)
+
+# 오름차순 정렬
+a.sort()
+print("오름차순 정렬: ", a)
+
+# 내림차순 정렬
+a.sort(reverse = True)
+print("내림차순 정렬: ", a)
