05de9aeb8a67cf9538e164b2ce343a73c3195b4f
yuuuzzzin <uiuiui118437@gmail.com>
2021-06-16 20:27:34 +0900

    2016년 문제 풀이

--- Programmers/Level 1/2016년.java
+++ Programmers/Level 1/2016년.java
@@ -1,42 +1,24 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+class Solution {
+    // 요일
+    static String[] day = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+    // 2016년의 1월-12월 일 수
+    static int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-public class Main {
+    public String solution(int a, int b) {
+        String answer;
+        int dayNum = 0;
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 도시의 개수
-        long[] dist = new long[n - 1]; // 거리
-        long[] cost = new long[n]; // 비용
-        long sum = 0;
-
-        // 거리 입력
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < dist.length; i++) {
-            dist[i] = Integer.parseInt(st.nextToken());
+        // 월에 따른 일 수 더하기
+        for(int i = 1; i < a; i++) {
+            dayNum += month[i - 1];
         }
 
-        // 기름값 입력
-        st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < cost.length; i++) {
-            cost[i] = Integer.parseInt(st.nextToken());
-        }
+        // 일 수 더하기
+        dayNum += b; 
 
-        // 현재보다 이전 도시의 기름값이 더 싸면 이전 도시의 기름값으로 갱신
-        long min = cost[0];
-        for(int i = 1; i < cost.length; i++) {
-            if(min < cost[i]) cost[i] = min;
-            else min = cost[i];
-        }
-
-        // 각각의 거리와 기름값을 더해 총합 구하기
-        for(int i = 0; i < dist.length; i++) {
-            sum += dist[i] * cost[i];
-        }
-
-        System.out.print(sum);
-        br.close();
+        // 요일 계산
+        answer = day[(dayNum + 4) % 7];
+    
+        return answer;
     }
 }
